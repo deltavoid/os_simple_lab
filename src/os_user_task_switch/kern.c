1db430819a6eff26bc7329608927a2474996be44
zqy <850074816@qq.com>
2018-07-27 12:44:09 +0800

    enable S mode interrupt

--- src/os_user_task_switch/kern.c
+++ src/os_user_task_switch/kern.c
@@ -36,18 +36,24 @@ void memset(void* s, uint8_t v, uint32_t len)
         a[i] = v;
 }
 
+#define EXTRACT_FIELD(val, which) (((val) & (which)) / ((which) & ~((which)-1)))
+#define INSERT_FIELD(val, which, fieldval) (((val) & ~(which)) | ((fieldval) * ((which) & ~((which)-1))))
+
 
 
 #define USER_STACK_SIZE 1024
 #define KERN_STACK_SIZE 1024
-//uint32_t taskA_kern_stack[KERN_STACK_SIZE];
+uint32_t taskA_user_stack[USER_STACK_SIZE];
+uint32_t taskB_user_stack[USER_STACK_SIZE];
+uint32_t taskA_kern_stack[KERN_STACK_SIZE];
 uint32_t taskB_kern_stack[KERN_STACK_SIZE];
 struct pushregs taskA_context;
 struct pushregs taskB_context;
 
 void taskA()
 {
-    while (1)  putstring("A");
+    //while (1)  putstring("A");
+    while (1) ;
 
 }
 
@@ -73,8 +79,8 @@ void trap(struct trapframe* tf)
     set_mtimecmp(mtimecmp += timebase);
     
 
-    if  (tf->cause == (0x80000000 | IRQ_M_TIMER))
-    //if  (0)
+    //if  (tf->cause == (0x80000000 | IRQ_M_TIMER))
+    if  (0)
     {
         if  (current == 1)
         {   current = 0;
@@ -97,18 +103,21 @@ void kern_entry()
     mtimecmp = timebase;
     set_mtimecmp(mtimecmp);
 
+    write_csr(mscratch, 0);
     write_csr(mtvec, &__alltraps);
     write_csr(mie, MIP_MTIP);
-    write_csr(mstatus, MSTATUS_MIE);
+    //write_csr(mstatus, MSTATUS_MIE);
+    
 
 
+    /*
     uint32_t sp0 = (uint32_t)(taskB_kern_stack + KERN_STACK_SIZE);
 
     uint32_t sp1 = sp0 - sizeof(struct trapframe);
     struct trapframe* tf = (struct trapframe*)sp1;
     memset(tf, 0, sizeof(struct trapframe));
-    //tf->status = MSTATUS_MIE;
-    tf->status = 0x00001880;
+    //tf->status = MSTATUS_MIE;  //              1000
+    tf->status = 0x00001880;  //  0001 1000 1000 0000
     tf->epc = &taskB;
     tf->gpr.sp = sp0;
 
@@ -116,11 +125,63 @@ void kern_entry()
     memset(context, 0, sizeof(struct pushregs));
     context->ra = &__trapret;
     context->sp = sp1;
+    */
+
+    uintptr_t mstatus = 0;
+    mstatus = INSERT_FIELD(mstatus, MSTATUS_MPP, PRV_S);
+    mstatus = INSERT_FIELD(mstatus, MSTATUS_MPIE, 1);
+    mstatus = INSERT_FIELD(mstatus, MSTATUS_SIE, 1);
+
+    /*
+    uint32_t taskB_sp0 = (uint32_t)(taskB_kern_stack + KERN_STACK_SIZE);
+
+    uint32_t taskB_sp1 = taskB_sp0 - sizeof(struct trapframe);
+    struct trapframe* taskB_tf = (struct trapframe*)taskB_sp1;
+    memset(taskB_tf, 0, sizeof(struct trapframe));
+    taskB_tf->status = mstatus;
+    taskB_tf->epc = &taskB;
+    taskB_tf->gpr.sp = (uint32_t)(taskB_user_stack + USER_STACK_SIZE);
+
+    memset(&taskB_context, 0, sizeof(struct pushregs));
+    taskB_context.ra = &__trapret;
+    taskB_context.sp = taskB_sp1;
+    */
+
+
+    uint32_t taskA_sp0 = (uint32_t)(taskA_kern_stack + KERN_STACK_SIZE);
+
+    uint32_t taskA_sp1 = taskA_sp0 - sizeof(struct trapframe);
+    struct trapframe* taskA_tf = (struct trapframe*)taskA_sp1;
+    memset(taskA_tf, 0, sizeof(struct trapframe));
+    taskA_tf->status = mstatus;
+    taskA_tf->epc = &taskA;
+    taskA_tf->gpr.sp = (uint32_t)(taskA_user_stack + USER_STACK_SIZE);
+
+
+    uintptr_t sp = taskA_sp1;
+
+    asm volatile("mv sp, %0 \n\t"
+                 "j __trapret \n\t"
+                 :
+                 : "r"(sp));
+
+    /*    asm volatile("la t0, 1f\n\t"
+                 "csrrw t0, mtvec, t0\n\t"
+                 "csrw pmpaddr0, %1\n\t"
+                 "csrw pmpcfg0, %0\n\t"
+                 ".align 2\n\t"
+                 "1: csrw mtvec, t0"
+                 :
+                 : "r"(pmpc), "r"(-1UL)
+                 : "t0");*/
+
+
+
     
 
     //while (1);
-    current = 1;
-    taskA();
+    /*current = 1;
+    taskA();*/
 
     
 
